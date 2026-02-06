b92407b2e1de3d2573d98775d2f48e997b14ff70
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-06 12:54:36 +0900

    Add ltrim function to remove leading whitespace

--- Sherlock-and-Array.cpp
+++ Sherlock-and-Array.cpp
@@ -99,6 +99,14 @@ string ltrim(const string &str) {
     return s;
 }
 
+string ltrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+    return s;
+}
 string rtrim(const string &str) {
     string s(str);
 
